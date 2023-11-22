bb6e13b38a0439bc1b5895c1e9cae2a90795b74e
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-11-22 10:15:43 +0900

    [Feat] Add neutral gray to palette, rename palette filename to fit convention

--- lib/gdsc_ys_color.dart
+++ lib/gdsc_ys_color.dart
@@ -1,5 +1,5 @@
 library gdsc_ys_color;
 
 export 'src/enum/index.dart' show GDSCColorTheme;
-export 'src/palette/palette.style.dart' show GDSCPalette;
+export 'src/palette/index.dart' show GDSCPalette;
 export 'src/theme/index.dart' show GDSCTheme;
