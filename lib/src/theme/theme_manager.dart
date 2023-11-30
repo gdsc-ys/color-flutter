18487f947c0ae7b6ef1755b36a9c4eb27ed5e49c
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-11-30 18:17:08 +0900

    chore: add document comments for thememanager

--- lib/src/theme/theme_manager.dart
+++ lib/src/theme/theme_manager.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../../gdsc_ys_color.dart';
 
+/// A class that manages the theme of the app.
+/// It is a ChangeNotifier, so it can be used with the Provider package.
+///
+/// ## Using GDSCThemeManager with Provider to support dynamic theming
+///
+/// You can use GDSCThemeManager with Provider to support dynamic theming.
+/// ** See code in examples/dyanmic_theme/lib/dynamic_theme_with_provider.dart **
 class GDSCThemeManager extends ChangeNotifier {
   GDSCThemeManager({
     GDSCColorTheme defaultColor = GDSCColorTheme.blue,
